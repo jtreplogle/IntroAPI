d06e8229edd64b896e358f3fc44518b52dad60cc
jreplogle Replogle <jreplogle Replogle>
2017-02-08 22:51:53 -0600

    Basic intro API

---
+++ IntroAPI/Controllers/ArtistController.cs
@@ -0,0 +1,71 @@
+﻿using IntroAPI.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    //Run the code ~ URL:  http://localhost:49347/api/artist  and http://localhost:49347/api/artist/5
+    public class ArtistController : ApiController
+    {
+        public ApplicationDbContext dbContext = new ApplicationDbContext();
+
+        [System.Web.Http.HttpGet]
+        public IEnumerable Get()
+        {
+            var products = dbContext.Artist;
+            return products;
+        }
+        //Get: api/Artist/5
+        public Artist Get(int id)
+        {
+            var artist = dbContext.Artist.Find(id);
+            return artist;
+        }
+
+        //Post: api/Artist
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage Post(Artist a)
+        {
+            if (a == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+            dbContext.Artist.Add(a);
+            dbContext.SaveChanges();
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+
+        //Delete: api/Artist/5
+        public void Delete(int id)
+        {
+            var artist = dbContext.Artist.Find(id);
+            if(artist.ArtistId != 0)
+            {
+                Artist artistToDelete = dbContext.Artist.Find(id);
+                dbContext.Artist.Remove(artist);
+                dbContext.SaveChanges();
+            }
+        }
+
+        //Put
+        [System.Web.Http.HttpPut]
+        public HttpResponseMessage Put(int id, Artist a)
+        {
+            var artist = dbContext.Artist.Find(id);
+            if (artist == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            artist.Name = a.Name;
+            dbContext.SaveChanges();
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+    }
+}
